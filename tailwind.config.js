337588fe196acb5295680d089964eb7a618c1788
wilhelmgroth <115974049+wilhelmgrothbl@users.noreply.github.com>
2023-03-02 23:53:00 +0100

    bunch of stuff now its goodnight

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  theme: {},
+  theme: {
+    extend:
+    {
+      fontFamily: {
+        'sans': ['Nunito', 'sans-serif'],
+      },
+    },
+  },
   variants: {},
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
   content: [
     './src/**/*.html',
     './src/**/*.component.ts'
